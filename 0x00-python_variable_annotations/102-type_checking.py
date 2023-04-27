ac442704bfe5c255777dfe8717f475a868c6dbe7
Joshdb-18 <joshuadami17@gmail.com>
2023-04-28 00:50:39 +0100

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """mypy validation"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
